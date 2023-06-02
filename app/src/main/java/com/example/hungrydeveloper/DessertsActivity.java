90e61ad5ab4800f31949735305272c28ab3956b1
Kritamate <Kritamate.bs@gmail.com>
2023-06-02 13:42:09 +0700

    add: dessertPage

---
+++ app/src/main/java/com/example/hungrydeveloper/DessertsActivity.java
@@ -0,0 +1,34 @@
+package com.example.hungrydeveloper;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+public class DessertsActivity extends AppCompatActivity {
+
+    ListView dessertList;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_desserts);
+
+
+        dessertList = findViewById(R.id.list_view_desserts);
+
+        Dish[] dessertDish = {
+                new Dish("Cocoa and blackcurrant cake", "Moist cake made with cocoa and fresh blackcurrant", 499),
+                new Dish("Mulberry and cardamom crumble", "Fresh mulberry and green cardamom topped with butter crumble", 699),
+                new Dish("Lemon scones with chilli jam", "Vegan scones served with chilli jam and fresh coconut cream", 599),
+                new Dish("Potato and banana vegan crepes", "Crispy vegan crepes filled with fresh potato and banana", 599),
+                new Dish("Cinnamon and treacle buns", "Crumbly buns made with cinnamon and dark treacle", 499),
+        };
+
+        ArrayAdapter<Dish> dessertAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1,dessertDish);
+        dessertList.setAdapter(dessertAdapter);
+
+
+    }
+}
--- app/src/main/java/com/example/hungrydeveloper/MainActivity.java
+++ app/src/main/java/com/example/hungrydeveloper/MainActivity.java
@@ -4,13 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     CardView starterCard;
     CardView mainCard;
+    CardView dessertsCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +21,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         starterCard = findViewById(R.id.card_view_starters);
         mainCard = findViewById(R.id.card_view_main);
+        dessertsCard = findViewById(R.id.card_view_desserts);
 
 
         mainCard.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent mainCoursesActivityIntent = new Intent(MainActivity.this,MainCoursesActivity.class);
+                Intent mainCoursesActivityIntent = new Intent(MainActivity.this, MainCoursesActivity.class);
                 startActivity(mainCoursesActivityIntent);
             }
         });
@@ -32,10 +36,30 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
 
-                Intent startActivityIntent = new Intent(MainActivity.this,StarterActivity.class);
+                Intent startActivityIntent = new Intent(MainActivity.this, StarterActivity.class);
                 startActivity(startActivityIntent);
 
 
+            }
+        });
+
+        dessertsCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent dessertsActivity = new Intent(MainActivity.this,DessertsActivity.class);
+                startActivity(dessertsActivity);
+
+            }
+        });
+
+        TextView emailTextView = findViewById(R.id.text_view_email);
+        emailTextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent launchEmailIntent = new Intent(Intent.ACTION_SENDTO);
+
+                launchEmailIntent.setData(Uri.parse("mailto:"+emailTextView.getText().toString()));
+                startActivity(launchEmailIntent);
             }
         });
     }
